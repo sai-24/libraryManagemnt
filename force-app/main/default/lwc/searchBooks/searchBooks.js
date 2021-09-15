3b8aa13c64d24b48d3c86afcb63392da9a41e3f4
sai-24 <saisantoshpanda143@gmail.com>
2021-09-15 14:32:51 +0530

    Search changes

--- force-app/main/default/lwc/searchBooks/searchBooks.js
+++ force-app/main/default/lwc/searchBooks/searchBooks.js
@@ -4,8 +4,8 @@ import getBooks from '@salesforce/apex/getBooksList.getBooks';
 const columns = [
 { label: 'Id', fieldName: 'Id' },
 { label: 'Book Name', fieldName: 'Book_Name__c', type: 'text' },
-{ label: 'Book status', fieldName: 'Book_Status__c', type: 'text' },
-{ label: 'Return Date', fieldName: 'Return_Date__c', type: 'date' }
+{ label: 'Book status', fieldName: 'Borrow__r[0].Book_Status__c', type: 'text' },
+{ label: 'Return Date', fieldName: 'Borrow__r[0].Returned_On__c', type: 'date' }
 
 ];
 export default class SearchBooks extends LightningElement {
