8d0e79f6f4744e8429ed1882b46889854c239840
sai-24 <saisantoshpanda143@gmail.com>
2021-09-27 13:01:41 +0530

    Borrow and Return is done

--- force-app/main/default/lwc/returnBook/returnBook.js
+++ force-app/main/default/lwc/returnBook/returnBook.js
@@ -1,7 +1,8 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire,track } from 'lwc';
 import getReturnedBooks from '@salesforce/apex/getBooksList.getReturnedBooks';
 import UpdateReturnBook from '@salesforce/apex/getBooksList.UpdateReturnBook';
 import { refreshApex } from '@salesforce/apex';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 const columns = [
 { label: 'Id', fieldName: 'Id' },
 { label: 'Book Name', fieldName: 'Book_Name__c', type: 'text' },
@@ -12,8 +13,32 @@ const columns = [
 export default class ReturnBook extends LightningElement {
     columns = columns;
     reslist=[];
-@wire(getReturnedBooks)
-returnbooks;
+    returnbooks;
+    error;
+   @track wiredReturnList=[];
+
+@wire(getReturnedBooks) returnHandler(result) {
+    this.wiredReturnList = result;    
+    if (result.data) {
+     
+      this.returnbooks = result.data;          
+      this.error = undefined;
+    } else if (result.error) {
+      this.error = result.error;
+      
+    }
+}
+// connectedCallback(){
+//     this.returnbookslist();
+// }
+// returnbookslist(){
+//     console.log('Start');
+//     getReturnedBooks().then(result=>{            
+//             this.returnbooks=result;
+//         console.log(result);
+//         }
+//         );
+// }
 getSelectedRow(event){
     const selectedRows = event.detail.selectedRows;          
     for (let i = 0; i < selectedRows.length; i++){
@@ -25,7 +50,16 @@ handlereturnClick(){
     UpdateReturnBook({
         ids:this.reslist
     }).then(result=>{
-         refreshApex(this.returnbooks);
+       
+        const evt = new ShowToastEvent({
+            title: 'Success',
+            message: 'Books Returned Sucessfully',
+            variant: 'success',
+            mode: 'dismissable'
+            });
+            this.dispatchEvent(evt);
+            //this.returnbookslist();
+            refreshApex(this.wiredReturnList);
     })
 
 }
--- force-app/main/default/lwc/searchBooks/searchBooks.js
+++ force-app/main/default/lwc/searchBooks/searchBooks.js
@@ -1,11 +1,12 @@
 import { LightningElement } from 'lwc';
 import getBooks from '@salesforce/apex/getBooksList.getBooks';
 import UpdateBook from '@salesforce/apex/getBooksList.UpdateBook';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 const columns = [
 { label: 'Id', fieldName: 'Id' },
 { label: 'Book Name', fieldName: 'Book_Name__c', type: 'text' },
 { label: 'Book status', fieldName: 'Book_Status__c', type: 'text' },
-{ label: 'Return Date', fieldName: 'Expected_Returned_Date__c', type: 'date' }
+{ label: 'Return Date', fieldName: 'Returned_Date__c', type: 'date' }
 ];
 export default class SearchBooks extends LightningElement {
 butvalue;
@@ -54,6 +55,13 @@ handleborrowClick(event){
     UpdateBook({
         ids:this.reslist
     }).then(result=>{
+        const evt = new ShowToastEvent({
+            title: 'Success',
+            message: 'Book Borrowed Sucessfully',
+            variant: 'success',
+            mode: 'dismissable'
+            });
+            this.dispatchEvent(evt);
         this.bookslist();
         console.log(result);
     })
